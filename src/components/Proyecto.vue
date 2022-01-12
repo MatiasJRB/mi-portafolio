7dd9273e543a2cd24fdcca44506929ab762f15bf
MatiasJRB <40368281+MatiasJRB@users.noreply.github.com>
2022-01-12 15:42:58 -0300

    main

--- src/components/Proyecto.vue
+++ src/components/Proyecto.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-card class="width: 100% min-width: 350px max-width: 350px"  flat bordered>
+    <q-card  class=""  flat bordered style="border-radius: 15px">
       <q-card-section vertical>
 
         <q-card-section class="q-pt-xs text-center q-mt-md">
@@ -9,35 +9,46 @@
               <q-separator color="dark"/>
             </div>
             <div class="col">
-              <div class="text-caption">{{fecha}}</div></div>
+              <div class="text-caption">{{ project.date }}</div></div>
             <div class="col q-mt-sm q-mx-md">
               <q-separator color="dark"/>
             </div>
           </div>
           <div class="text-h6 text-dark  q-my-sm">
-            {{nombre}}
+            {{project.title}}
+          </div>          
+          <div class=" text-dark full-width q-my-sm">
+            {{project.description}}
           </div>          
         </q-card-section>
 
-        <q-card-section horizontal>
-          <div class="row q-pa-xs" style="margin: 0px">
-            <div class="col-12 col-md flex flex-center">
-              <q-img
-                class="rounded-borders "
-                style="max-width: 400px"            
-                :src="imagen"
-                spinner-color="dark"
-                :key="transition"
-                :transition="transition"
-                contain
+        <q-card-section horizontal >
+          <div class="row q-pa-xs full-width" style="margin: 0px">
+            <div class="col-md-12 col-xs-12 flex flex-center">
+              <q-carousel
+                @click.stop=""
+                swipeable
+                animated
+                v-model="slide"
+                class="full-width"
+                thumbnails
+                infinite
+                
+                style="border-radius: 15px"
               >
-                <div class="absolute-bottom text-caption text-center" >
-                  Contain
-                </div>
-              </q-img>
+              <q-carousel-slide
+               class="full-width"
+                v-for="(image,index) in project.images"
+                :key="index"
+                :name="index"
+                :img-src="image"
+                style="border-radius: 15px"
+               />
+
+              </q-carousel>
             </div>
-            <div class="col-12 col-md q-ma-md">
-              <div class="text-body1  text-dark " v-html="contenido">
+            <div class="col-md-12 col-xs-12 col-md q-my-sm" v-if="!showMore">
+              <div class="q-mx-md " v-html="project.content">
                 <!-- {{descripcion}} -->
               </div>
             </div>
@@ -45,23 +56,30 @@
           
           
         </q-card-section>
-        <q-card-section vertical v-show="mostrar_saber_mas">
-          <div class="row q-pa-xs flex flex-center " style="margin: 0px">
-            <q-btn flat class="bg-accent full-width" borderer text-color="dark" label="Saber más">
-               </q-btn>
+        <q-card-section class="row">
+          <div  v-for="(media,index) in project.media" :key="index">
+            <q-btn class="q-ml-sm" :label="media.title" @click="handleMedia(media)" rounded/>
           </div>
         </q-card-section>
+        <q-separator color="accent"/>
+
+        <q-card-actions vertical v-show="showMore">
+          <div class="row q-pa-xs flex flex-center " style="margin: 0px">
+            <q-btn @click.stop="goToProjectPage" flat class="bg-accent full-width" borderer text-color="dark" label="Saber más" rounded>
+               </q-btn>
+          </div>
+        </q-card-actions>
       </q-card-section>
 
-      <q-separator color="accent"/>
+      
 
       
 
       <q-card-actions class="q-ml-md q-my-sm">
-        <q-badge outline color="dark" text-color="dark" label="Status: in progress" class="q-mx-xs" /> 
-        <q-badge outline color="dark" label="JavaScript" class="q-mx-xs"/>
-        <q-badge outline color="dark" label="GraphQL" class="q-mx-xs"/>
-        <q-badge outline color="dark" label="CSS" class="q-mx-xs"/>
+        <q-badge outline color="dark" text-color="dark" :label="'Status:' + project.status" class="q-mx-xs" />
+        <div class="" v-for="(tag,index) in project.tags" :key="index" >
+          <q-badge outline color="dark" :label="tag" class="q-mx-xs" />
+        </div>
       </q-card-actions>
     </q-card>
 </template>
@@ -72,11 +90,6 @@ import Vue from 'vue'; // es6 syntax
 
 export default({
   props:{
-    mostrar_saber_mas:
-    {
-      type: String,
-      default: "false"
-    },
     transition:
     {
       type: String,
@@ -85,7 +98,7 @@ export default({
     nombre:
     {
       type: String,
-      default: "NOMBRE DEL PROYECTO"
+      default: "NOMBRE DEL projectO"
     },  
     imagen:
     {
@@ -97,9 +110,9 @@ export default({
       type: String,
       default: "4 de Junio, 2021"
     },  
-    contenido: {
-      type: String,
-      default: ""
+    project: {
+      type: Object,
+      required: true 
     },
     descripcion:
     {
@@ -110,11 +123,20 @@ export default({
   data()
   {
     return{
-      mostrar_info_detalleada: false
+      showMore: false,
+      mostrar_info_detalleada: false,
+      slide: 0
+    }
+  },
+  methods: {
+    handleMedia(media) {
+      if (media.type === 'video') {
+      }
     }
   },
   mounted () {
-    console.log(this.contenido)
+    console.log(this.project)
+    this.showMore = this.project.showMore
   }
 })
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,7 +12,7 @@
                 <div class="row justify-center  q-my-xl " style=" ">
                   <div >
                     <div class="text-h6 text-weight-thin">
-                      Fullstack developer
+                      Full Stack Engineer
                     </div>
                     <div class="text-h3 text-weight-bold q-my-sm">
                       Rios <br>Matias
@@ -31,24 +31,11 @@
                         </div>
                       </div>                  
                     </div>
-                    <!--div class="text-overline full-width q-my-sm">
-                      Disponible para relocación
-                    </div-->
                   </div>
                   
                 </div>            
                 
-              </q-card>  
-              <!--div class="row justify-center relative-bottom q-pb-xs q-mt-xl">
-                <div class="row justify-center">
-                  Connect
-                </div>
-                <div class="row justify-center">
-                  <q-avatar>
-                    <img src="https://cdn.quasar.dev/img/avatar.png">
-                  </q-avatar>
-                </div>
-              </div-->  
+              </q-card> 
               
                     
           </div>
@@ -63,7 +50,7 @@
               <div v-for="project in projects" :key="project.id" class="col-12 col-md text-center">
 
                 <Proyecto
-                :contenido="project.content"></Proyecto>
+                :project="project"/>
 
               </div>
             </div>
@@ -247,26 +234,27 @@
 
                 </div>
                 <div class="text-body2 q-my-md">
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Soy Ingeniero en Computación graduado de la Universidad Nacional del Sur, y me encuentro trabajando a jornada completa. Suelo aceptar diferentes trabajos freelance que me obligan a aprender nuevas cosas de manera autodidacta. Me apasiona el entrenamiento y el deporte (guerrero educado), la creación y edición de fotos y videos y determinados e-sports.
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Soy Ingeniero en Computación graduado de la Universidad Nacional del Sur, y me encuentro trabajando a jornada completa. Suelo aceptar diferentes trabajos freelance que me obligan a aprender nuevas herramientas de manera autodidacta. Me apasiona el entrenamiento y el deporte (guerrero educado), la creación y edición de fotos y videos y determinados e-sports.
                   <br>
                 </div>
               </div>
             </div>
 
             <!-- Projects -->
-            <div class="row justify-around q-ma-lg" style=" ">
-              <div class="col">
+            <div class="row justify-around q-ma-lg" >
+              <div class="col" >
                 <div class="text-overline">
-                  PROYECTOS
+                  PORTFOLIO
                 </div>
-                <div v-for="project in projects" :key="project.id">
+                <div v-for="project in projects" :key="project.id"
+                  class="q-mt-md" >
 
                   <Proyecto
-                  :imagen="project.imagen"
-                  :contenido="project.content"
-                  :fecha="project.fecha"
-                  :descripcion="project.descripcion"
-                  :nombre="project.nombre"></Proyecto>
+                    :imagen="project.imagen"
+                    :project="project"
+                    :fecha="project.fecha"
+                    :descripcion="project.descripcion"
+                    :nombre="project.nombre"/>
 
                 </div>
               </div>
@@ -554,15 +542,15 @@ export default {
     
     this.funciones= new Vue(ModuloFunciones);
     this.network = new Vue(ModuloNetwork)
-    this.network.getProjects()    
-      .then(response=>response.json())
-      .then(resp=> {
-        console.log(resp)
-        this.projects = resp["items"]
-        // console.log('Los proyectos son estos: ')
-        // console.log(this.projects)        
-    });
-    // this.setear_projects()
+    // this.network.getProjects()    
+    //   .then(response=>response.json())
+    //   .then(resp=> {
+    //     console.log(resp)
+    //     this.projects = resp["items"]
+    //     // console.log('Los proyectos son estos: ')
+    //     // console.log(this.projects)        
+    // });
+    this.setear_projects()
     this.$root.$on('listener_principal', (param) => this.listener(param));
     console.log("Nope, u won't find any errors here :)")
     //this.funciones.alerta_positiva_home("Bienvenido");
@@ -577,14 +565,182 @@ export default {
   methods:
   {
     setear_projects(){
-      this.projects = [
+      this.projects = []
+      
+      let project = {}
+      
+      // PROYECTO LEAP MOTION
+            
+      project = {}
+      project.title =  "PROYECTO LEAP MOTION"
+      project.description =  "Un proyecto muy interesante para la materia Ingeniería de aplicaciones correspondiente al segundo cuatrimestre de 4to año de la carrera, a cargo de Luján Ganuza."
+      project.content = ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Se nos fue la mano con la cantidad de horas invertidas en este proyecto, que consistía en una experiencia de realidad virtual controlada mediante gestos de las manos. Un servidor, un Leap Motion, un smarthphone, mucho tiempo y muchas ganas para poder lograr lo demostrado en el trailer.'
+      project.media = [
         {
-          "nombre": "Going Back Home",
-          "descripcion": "Projecto para la material computación gráfica",
-          "fecha": "4 de junio, 2020",
-          "imagen": null
+          type: 'url',
+          title: 'Luján Ganuza',
+          src: 'http://vyglab.cs.uns.edu.ar/webpage/index.php/en/people/m-lujan-ganuza'
+        },
+        {
+          type: 'video',
+          title: 'Trailer',
+          src: 'https://www.youtube.com/embed/Gon-pctdt1g'
         }
       ]
+      project.date =  "27 de noviembre, 2019"
+      project.status =  "Terminado"
+      project.tags =  ['Leap Motion','C#','Unity3D','Realidad virtual','Mobile']      
+      project.images = [
+        'https://matiasjrb.com.ar/images/leapmotion.jpeg',
+        'https://matiasjrb.com.ar/images/proyectoiap.jpg'
+      ]
+      project.videos = [
+        'https://www.youtube.com/embed/Gon-pctdt1g'
+      ]
+      project.showMore = false
+      this.projects.push(project)
+
+
+      // PARRILLA AUTOMATICA
+
+      project = {}
+      project.title =  "PARRILLA AUTOMÁTICA"
+      project.description =  "Un proyecto para la materia Sistemas embebidos correspondiente al segundo cuatrimestre de 4to año de la carrera, a cargo de José Moyano. Basado en la idea de mi amigo y compañero de proyectos Bruno Mignucci, una parrilla automática de madera que todos queremos que Brunito traslade a fierro y braza."
+      project.content = ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fue desarrollado en Java a lo largo de 40 días utilizando como base de datos archivos .xlsx de Excel. El programa funciona con lectora de código de barras y es altamente adaptable a todo tipo de comercios que deseen eliminar el papel de su día a día, para mayor comodidad y seguridad.'
+      project.media = [
+        {
+          type: 'url',
+          title: 'José Moyano',
+          src: 'https://www.facebook.com/jhmoyano'
+        },
+        {
+          type: 'url',
+          title: 'Bruno Mignucci',
+          src: 'https://www.facebook.com/brunomignucci'
+        },
+        {
+          type: 'video',
+          title: 'Demostración',
+          src: 'https://www.youtube.com/watch?v=jU1Nd6McBcM&t=29s'
+        },
+      ]
+      project.date =  "27 de noviembre, 2019"
+      project.status =  "Terminado"
+      project.tags =  ['Java']      
+      project.images = [
+        'https://matiasjrb.com.ar/images/parri.png',
+        'https://matiasjrb.com.ar/images/petshop/portada.jpg'
+      ]
+      project.videos = [
+        'https://matiasjrb.com.ar/documentos/redes/vid.mp4'
+      ]
+      project.showMore = false
+      this.projects.push(project)
+
+      // PRIMER SOFTWARE VENDIDO
+
+      project = {}
+      project.title =  "PRIMER SOFTWARE VENDIDO 'GEOME7RIC 1.0'"
+      project.description =  "Software hecho a medida para PetShop Undiano: inventario para control de stock, control de ventas, proveeduría y clientela"
+      project.content = ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fue desarrollado en Java a lo largo de 40 días utilizando como base de datos archivos .xlsx de Excel. El programa funciona con lectora de código de barras y es altamente adaptable a todo tipo de comercios que deseen eliminar el papel de su día a día, para mayor comodidad y seguridad.'
+      project.media = [
+        {
+          type: 'pdf',
+          title: 'Manual de usuario',
+          src: 'https://matiasjrb.com.ar/documentos/inventario/manual.pdf'
+        }
+      ]
+      project.date =  "29 de junio, 2019"
+      project.status =  "Terminado"
+      project.tags =  ['Java']      
+      project.images = [
+        'https://matiasjrb.com.ar/images/petshop/logo.jpg',
+        'https://matiasjrb.com.ar/images/petshop/portada.jpg'
+      ]
+      project.videos = [
+        'https://matiasjrb.com.ar/documentos/redes/vid.mp4'
+      ]
+      project.showMore = false
+      this.projects.push(project)
+
+      // RASTREO DNS
+      project = {}
+      project.title =  "RASTREO DNS"
+      project.description =  "Script en Python hecho para la materia Redes y Teleprocesamiento solicitado por Sergio Davicino"
+      project.content = ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Se utilizó la libreria Scapy y servidores de Google. Las características del script son las siguientes. Argumentos y opciones de entrada: El FQDN sobre la cual se va a realizar la traza. Las opciones ”-h” o ”- -help” que hacen que el script retorne una ayuda de la forma de invocación. Validación de la entrada: Se debe validar que el FQDN pasado como par´ametro tenga resolución de DNS antes de iniciar la traza.Salida:En el caso de que las opciones de entrada no se ajusten a lo esperado por el script, debe imprimir por pantalla la ayuda. Para cada uno de los subdominios, el script debe mostrar por pantalla el nombre y la dirección de cada servidor de nombres (ver ejemplo). Al finalizar la traza debe mostrar todos los RRs asociados al FQDN (ver ejemplo).Ejecución:El script debe obtener los nombres de los TLD haciendo una consulta a los servidores de DNS configurados en el equipo (archivo /etc/resolv.conf ), y como última instancia, debe usar los servidores de Google. El script no puede invocar ningún comando exterior, solo librerías de Python y Scapy. En cada uno de los pasos, la resolución de nombres para presentar por pantalla la información, se llevará a cabo usando los servidores que se citan en el primer punto.'
+      project.media = [
+        {
+          type: 'url',
+          title: 'Script',
+          src: 'https://matiasjrb.com.ar/documentos/redes/RiosMatias105434'
+        }
+      ]
+      project.date =  "14 de junio, 2019"
+      project.status =  "Aprobado"
+      project.tags =  ['Python', 'DNS', 'FQDN ',  'UNS']      
+      project.images = [
+        'https://matiasjrb.com.ar/images/redes/dns.jpg'
+      ]
+      project.videos = [
+        'https://matiasjrb.com.ar/documentos/redes/vid.mp4'
+      ]
+      project.showMore = true
+      this.projects.push(project)
+
+      // CURSO DE REACT
+      project = {}
+      project.title =  "CURSO EATI2019 'INTRODUCCIÓN A REACT'"
+      project.description =  "Curso de verano dictado por la Escuela de Actualización en Tecnologías de Informática (EATI) a cargo de Mariano Tucat"
+      project.content = ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Un curso en donde programamos en JavaScript haciendo uso de NodeJS y React Dev Tools. La evaluación consistió en elaborar una pequeña página web de comidas utilizando las facilidades que provee REACT.'
+      project.media = [
+        {
+          type: 'pdf',
+          title: 'Certificado de aprobación',
+          src: 'https://matiasjrb.com.ar/documentos/eati2019/certificado.pdf'
+        },
+        {
+          type: 'url',
+          title: 'Mariano Tucat',
+          src: 'https://www.linkedin.com/in/marianotucat/?originalSubdomain=ar'
+        },
+      ]
+      project.date =  "7 de Abril, 2019"
+      project.status =  "Aprobado"
+      project.tags =  ['React', 'NodeJS', 'UNS', 'EATI2019']      
+      project.images = [
+        'https://matiasjrb.com.ar/images/eati2019/logo.jpg',
+        'https://matiasjrb.com.ar/images/goingbackhome.png'
+      ]
+      project.videos = [
+        'https://www.youtube.com/embed/AuBwgJXerKc'
+      ]
+      project.showMore = false
+      this.projects.push(project)
+
+      // ALIEN
+      project = {}
+      project.title =  "Going Back Home"
+      project.description =  "Proyecto final de materia Computación gráfica"
+      project.media = [
+        {
+          type: 'video',
+          title: 'Trailer',
+          src: 'https://www.youtube.com/watch?v=AuBwgJXerKc'
+        }
+      ]
+      project.date =  "29 de junio, 2018"
+      project.status =  "Terminado"
+      project.tags =  ['C++', 'Unity 3D', 'UNS']
+      project.images = [
+        'https://matiasjrb.com.ar/images/goingbackhome.png',
+        'https://matiasjrb.com.ar/images/goingbackhome.png'
+      ]
+      project.videos = [
+        'https://www.youtube.com/embed/AuBwgJXerKc'
+      ]
+      project.showMore = false
+      this.projects.push(project)
+      
     },
     showAddNewPost(){
       console.log('Apretaste el 7')
