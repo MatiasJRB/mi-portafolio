a3bebd9c0852e59a9ae3eb4d916a305c81428ea1
MatiasJRB <40368281+MatiasJRB@users.noreply.github.com>
2022-04-18 23:57:10 -0300

    fix hovers

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -2,18 +2,18 @@
   <q-page  class="flex flex-center" :style=" mobile? '  ' : 'padding-left: 130px'" >
     <div class="full-width  flex flex-center " :style=" $q.screen.xl? 'height: 100vh' : ''">
       <div class="row justify-start full-width  ">
-        <q-card  
-          flat           
+        <q-card
+          flat
           @click="handleShowProject(a)"
           v-ripple class="col-xs-6 col-sm-4  col-xs-6 col-md-2 col-xl-2"
           v-for="a in projects" :key="a.title"  :style="getCardStyle(a)"  >
-          <div v-if="!mobile && a.images &&  !hover[a.images[0]] && !showProject" class="absolute bg-primary full-width full-height" style="opacity: 0.5; z-index: 2"></div>
+          <div v-if="!mobile && !hover[a.id] && !showProject" class="absolute bg-primary full-width full-height" style="opacity: 0.8; z-index: 2"></div>
           <q-img  class="full-height" fit="fill" :src="a.cover" >
-          
+
             <div class="absolute-bottom-right bg-transparent" v-if="a.geome7ric">
               <img src="../assets/geome7ric_logo.svg" width="55px"  />
             </div>
-          </q-img> 
+          </q-img>
           <q-tooltip
             v-model="hover[a.id]"
             anchor="center middle"
@@ -21,7 +21,7 @@
             class="bg-secondary cursor-pointer	"
           >
             <div class="cursor-pointer " style="cursor: pointer">
-              <div class="bg-primary text-white shadow-2 flex flex-center cursor-pointer	" 
+              <div class="bg-primary text-white shadow-2 flex flex-center cursor-pointer	"
                 style="border-radius: 50%; height: 90px; width: 90px" >
                 <div class="cursor-pointer	">
                   <div class="text-center cursor-pointer" style="margin-top:px; font-size: 14px">
@@ -31,13 +31,13 @@
               </div>
             </div>
           </q-tooltip>
-          
-        
+
+
         </q-card>
-      </div> 
+      </div>
       <q-dialog  :maximized="mobile" class="bg-dark "  v-model="showProject"  position="bottom">
         <div class="bg-dark" style="width: 100vh">
-          
+
           <div class="row justify-start " >
               <q-btn color="white" flat icon="close" class="flex flex-center q-ma-md" @click="showProject =! showProject"/>
           </div>
@@ -46,9 +46,9 @@
             swipeable
             control-color="white"
             v-model="slide"
-            arrows 
+            arrows
             class="full-width bg-dark"
-            infinite            
+            infinite
           >
             <q-carousel-slide
               class=" "
@@ -67,34 +67,34 @@
             </div>
             <div class="text-h6 q-my-sm">
               {{projectToShow.title}}
-            </div>          
+            </div>
             <div class="" v-html="projectToShow.description">
               <!-- {{descripcion}} -->
-            </div>     
+            </div>
           </div>
           <div class="col-md-12 col-xs-12 col-md q-my-sm" >
             <div class="q-mx-md text-white" v-html="projectToShow.content">
             </div>
           </div>
-          <div 
-            v-for=" document in projectToShow.documents" 
-            :key="document.id" 
+          <div
+            v-for=" document in projectToShow.documents"
+            :key="document.id"
             class="q-ma-md">
-            <q-btn 
+            <q-btn
               no-caps
-              color="white" 
+              color="white"
               text-color="primary" :label="document.name" @click="open(document.url)" />
           </div>
           <div class="">
             <q-card-section class="row">
               <div  v-for="(media,index) in projectToShow.media" :key="index">
-                <q-btn 
+                <q-btn
                 no-caps
-                color="white" 
+                color="white"
                 text-color="primary" :label="media.title" @click="open(media.src)" />
               </div>
             </q-card-section>
-          </div> 
+          </div>
           <div class="">
             <q-card-actions class="q-ml-md " style="">
               <q-badge outlined  color="white" text-color="dark" :label="'Status: ' + projectToShow.status" class="q-mx-xs" />
@@ -103,11 +103,11 @@
               </div>
             </q-card-actions>
           </div>
-        </div> 
-       
-      </q-dialog>     
+        </div>
+
+      </q-dialog>
     </div>
-    
+
   </q-page>
 </template>
 
@@ -132,7 +132,7 @@ export default {
         return this.$store.state.articles.articles
       }
     },
-    
+
     mobile () {
       let ret = false
       ret = this.$q.screen.xs || this.$q.screen.sm || this.$q.screen.width < 1050
@@ -164,7 +164,7 @@ export default {
   },
   async mounted () {
     await this.$store.dispatch('articles/getArticles')
-      
+
   }
 }
 </script>
