b2c3174ee383df4292924367676d41ea9dca7315
Matias Rios <matiasjriosb@gmail.com>
2024-06-16 12:30:47 -0300

    Improve firebase config

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,23 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+// Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "personal-web-634c1.firebaseapp.com",
-  projectId: "personal-web-634c1",
-  storageBucket: "personal-web-634c1.appspot.com",
-  messagingSenderId: "125534217272",
-  appId: "1:125534217272:web:5df6c71954a11613cbb2cd",
-  measurementId: "G-PCV6PVTCRR",
-};
+const apiKey = process.env.FIREBASE_API_KEY;
+let app, analytics;
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+if (apiKey) {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "personal-web-634c1.firebaseapp.com",
+    projectId: "personal-web-634c1",
+    storageBucket: "personal-web-634c1.appspot.com",
+    messagingSenderId: "125534217272",
+    appId: "1:125534217272:web:5df6c71954a11613cbb2cd",
+    measurementId: "G-PCV6PVTCRR",
+  };
+
+  // Initialize Firebase
+  app = initializeApp(firebaseConfig);
+  analytics = getAnalytics(app);
+}
 
 export { app, analytics };
